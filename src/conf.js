872c1bcf530bc0089ae0c8a9ad3b15b42a24f355
Chuc Nguyen <nhiennguyen1605@gmail.com>
2022-06-02 12:11:24 +0700

    first commit

---
+++ src/conf.js
@@ -0,0 +1,18 @@
+export const items = [
+  {
+    sku: 'cinzel_100',
+    value: 100,
+  },
+  {
+    sku: 'cinzel_200',
+    value: 200,
+  },
+  {
+    sku: 'cinzel_300',
+    value: 300,
+  },
+  {
+    sku: 'cinzel_400',
+    value: 400,
+  },
+];
---
+++ src/subf.js
@@ -0,0 +1,10 @@
+export const subs = [
+  {
+    sub: 'cinzel_500',
+    value: 500,
+  },
+  {
+    sub: 'cinzel_1000',
+    value: 1000,
+  },
+];
